a2f9188695ab295a721eaacf08d25cc6e31b8232
Ron Winchester <RonWinchester@yandex.ru>
2021-11-04 15:01:45 +0300

    fix: фиксация состояния

---
+++ next.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer:/\.(js|ts)x?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              }
+            }
+          }
+        }
+      ],
+    });
+
+    config.plugins.push(new webpack.DefinePlugin({
+      'process.env.ORIGIN': JSON.stringify(process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://lubimovka.ru'),
+    }));
+
+    return config;
+  },
+  rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/api/:path*',
+          destination: 'https://real-backend-url/:path*',
+        },
+      ],
+    };
+  },
+};
