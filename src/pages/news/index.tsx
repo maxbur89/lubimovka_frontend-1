c011fb0cb646171751e6446ee0642cef8058d112
PavelEremeev <pe19950520@yandex.ru>
2022-01-23 20:44:45 +0300

    fix: Исправление скролла + проверка лимита.

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -13,8 +13,20 @@ const News = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => 
   const [hasMore, setHasMore] = useState(true);
   const [count, setCount] = useState<number>(0);
 
+
+  const getNumberOfNews = async () => {
+    fetchNewsList(0, 0)
+      .then(dataNumbers => {
+        const countOfNews = dataNumbers?.count;
+        return Number(countOfNews);
+      }).then(dataNumbers => {
+        setCount(dataNumbers);
+      })
+  }
+
   const getMoreNews = async () => {
-    fetchNewsList(5, offset)
+    setOffset(offset + 3)
+    fetchNewsList(3, offset)
       .then(data => {
         const results = data?.results;
 
@@ -34,22 +46,15 @@ const News = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => 
       .catch(error => error)
   }
 
-  const getNumberOfNews = async () => {
-    fetchNewsList(0, 0)
-      .then(dataNumbers => {
-        const countOfNews = dataNumbers?.count;
-        return Number(countOfNews);
-      }).then(dataNumbers => {
-        setCount(dataNumbers);
-        console.log(dataNumbers)
-      })
-  }
-
-  getNumberOfNews()
 
   useEffect(() => {
-    setHasMore(setCount > setNews.length ? true : false)
-  }, [news])
+    console.log(count)
+    console.log(news?.length)
+    getNumberOfNews()
+    if (news) {
+      setHasMore(count > news.length ? true : false)
+    }
+  }, [news, count])
 
   return (
     <AppLayout>
@@ -75,7 +80,7 @@ const fetchNewsList = async (limit: number, offset: number) => {
 
 export const getServerSideProps: GetServerSideProps<PaginatedNewsItemListList> = async () => {
 
-  const newsList = await fetchNewsList(5, 0);
+  const newsList = await fetchNewsList(3, 0);
 
   if (!newsList) {
     return {
